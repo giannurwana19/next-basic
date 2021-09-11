329d2f50f7fe85ad44cd7f900ae28b79e9bed683
giannurwana19 <giannurwana19@gmail.com>
2021-09-11 14:43:25 +0700

    Halaman 404

---
+++ pages/404.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+export default function Custom404() {
+  return (
+    <div>
+      <h1 className="title-not-found">Ooops... </h1>
+      <h1 className="title-not-found">
+        Halaman yang anda carii tidak ditemukan
+      </h1>
+    </div>
+  );
+}
