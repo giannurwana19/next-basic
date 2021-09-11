26bdd5b942c59f4a12dbc4342282211f4dc32d8f
giannurwana19 <giannurwana19@gmail.com>
2021-09-11 15:20:48 +0700

    dynamic routes

--- pages/users/[id].js
+++ pages/users/[id].js
@@ -1,10 +1,14 @@
+import { useRouter } from 'next/router';
 import Header from '../../components/Header';
 
 export default function Detail() {
+  const router = useRouter();
+  const { id } = router.query;
+
   return (
     <div>
       <Header />
-      <h1>detail users page</h1>
+      <h1>detail users {id}</h1>
     </div>
   );
 }
