262bd9603549ba7f78171fe27570023a862b99ce
giannurwana19 <giannurwana19@gmail.com>
2021-09-11 14:47:33 +0700

    auto redirection router.push

--- pages/404.js
+++ pages/404.js
@@ -1,6 +1,16 @@
-import React from 'react';
+import { useRouter } from 'next/dist/client/router';
+import { useEffect } from 'react';
 
 export default function Custom404() {
+  const router = useRouter();
+
+  useEffect(() => {
+    setTimeout(() => {
+      console.log('dipanggil setelah 2 detik');
+      router.push('/');
+    }, 2000)
+  }, []);
+
   return (
     <div>
       <h1 className="title-not-found">Ooops... </h1>
