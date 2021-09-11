96e9a4af2fa3e0be13c4e8510642f8ca9eb33597
giannurwana19 <giannurwana19@gmail.com>
2021-09-11 15:14:37 +0700

    optimalisasi image

--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,18 @@
 import Layout from '../components/Layout';
 import styles from '../styles/Home.module.css';
+import Image from 'next/image';
 
 export default function Home() {
   return (
     <Layout pageTitle="Home page">
+      <Image src="/profile.png" width={200} height={200} alt="profile" />
+      <img src="/profile.png" width={200} height={200} alt="profile" />
       <h1 className={styles['title-homepage']}>Welcome Gian</h1>
     </Layout>
   );
 }
+
+// DOCS
+
+// component Image Next js akan mengkompres image kita sehingga jadi lebih kecil
+// dan juga ada fitur lazy load, hanya akan load ketika dibutuhkan
